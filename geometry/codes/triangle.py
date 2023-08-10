acf6c8000fe5504f9c7f31489a69b31b903f0426
rusna2004 <141239038+rusna2004@users.noreply.github.com>
2023-08-10 12:28:05 +0530

    Add files via upload

--- geometry/codes/triangle.py
+++ geometry/codes/triangle.py
@@ -1,7 +1,8 @@
 #Find the distance between I(incentre) and BC.
 import numpy as np
-I=(1/(np.sqrt(37) + 4 + np.sqrt(61)))*np.array([[np.sqrt(61) - 16 - 3*np.sqrt(37)],[-np.sqrt(61) + 24 - 5*np.sqrt(37)]]) #Here I is incentre (point of intersection of angle bisectors)
-n = np.array([[11], [1]]) #n is normal vector 
-nT = n.T #taking transpose of n
-r = abs((nT @ I) + 38)/(np.linalg.norm(n)) #r is distance between I and BC (nI+38=0, n and I are vectors)
+B = np.array([[-4],[6]])  
+I=(1/(np.sqrt(37) + 4 + np.sqrt(61)))*np.array([[np.sqrt(61) - 16 - 3*np.sqrt(37)],[-np.sqrt(61) + 24 - 5*np.sqrt(37)]])    #Here I is incentre (point of intersection of angle bisectors)
+n = np.array([[11], [1]])   #n is normal vector 
+nT = n.T   #taking transpose of n
+r = abs((nT @ I) - (nT @ B))/(np.linalg.norm(n))   #r is distance between I and BC (nT.I - nT.B=0, n and I are vectors)
 print(r)
